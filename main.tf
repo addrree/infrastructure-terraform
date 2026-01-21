7d43c7426f5d68398a47f67b5e472334508ba481
Andrey Ilin <145039836+addrree@users.noreply.github.com>
2026-01-21 17:07:51 +0300

    delete secgroup

--- main.tf
+++ main.tf
@@ -46,29 +46,7 @@ variable "secgroup_name" {
 
 # Можно оставить только 22, а порты приложения открывать не обязательно,
 # если не требуют доступ снаружи. Но часто проверяют, что сервис слушает.
-resource "openstack_networking_secgroup_rule_v2" "ssh" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = data.openstack_networking_secgroup_v2.sg.id
-}
 
-resource "openstack_networking_secgroup_rule_v2" "ports" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 8092
-  port_range_max    = 8095
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = data.openstack_networking_secgroup_v2.sg.id
-}
-
-data "openstack_networking_secgroup_v2" "sg" {
-  name = var.secgroup_name
-}
 
 data "openstack_images_image_v2" "img" {
   name        = var.image_name
